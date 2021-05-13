d7339ccb48f3a51c392e39dae47f22906a8cfdfd
Nick Foden <nickfoden@gmail.com>
2021-05-13 16:43:31 -0400

    fade in and out the sidebar (#34)

--- src/components/header/ButtonLeft.js
+++ src/components/header/ButtonLeft.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
-import Hamburger from '../../images/hamburger.svg';
-
+import React from "react";
+import styled from "styled-components";
+import Hamburger from "../../images/hamburger.svg";
 
 const HumburgerButton = styled.img`
-    margin-top: 2rem;
-    width: 40px;
-    height: 40px;
-    cursor: pointer;
-`
+  margin-top: 2rem;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  margin-left: ${({ expanded }) => (expanded ? "0px" : "180px")};
+`;
 
-const ButtonLeft = () => {
-    return (
-        <HumburgerButton src={Hamburger}/>
-    )
-}
+const ButtonLeft = ({ expanded, handleExpand }) => (
+  <HumburgerButton expanded={expanded} src={Hamburger} onClick={handleExpand} />
+);
 
-export default ButtonLeft
-
+export default ButtonLeft;
