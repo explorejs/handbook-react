8725d0ec7676d4f3d929d905a32275feaca2e456
Nick Foden <nickfoden@gmail.com>
2021-11-01 19:43:11 -0400

    initial cypress setup

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('/')
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="about"]').click()
+
+    // The new url should include "/about"
+    cy.url().should('include', '/about')
+
+    // The new page should contain an h1 with "About page"
+    cy.get('h1').contains('About')
+
+    cy.get('[href="/register"]').click()
+
+    cy.contains('Create an account')
+    
+    cy.get('[href="/login"]').click()
+  })
+})
